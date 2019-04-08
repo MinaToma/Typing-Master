a5c5628a5f936611dee6b4fc7b634b865ef12f03
MinaToma <39103197+MinaToma@users.noreply.github.com>
2019-04-08 18:14:08 +0200

    Main player and missles added with colliders

---
+++ Typing-Master/Assets/MissleMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D missle;
+	void Start ()
+    {
+        missle = GetComponent<Rigidbody2D>();
+        missle.velocity = transform.up * speed;
+    }	
+}
---
+++ Typing-Master/Assets/playerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    
+    private Rigidbody2D playerRigid2d;
+    public GameObject shot;
+    public Transform ship;
+    
+    void Start ()
+    {
+        playerRigid2d = GetComponent<Rigidbody2D>();
+    }
+	
+	void Update ()
+    {
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(vKey))
+            {
+                Instantiate(shot, ship.position, ship.rotation);
+            }
+        }
+    }
+
+    void RotateLeft ()
+    {
+        transform.Rotate(Vector3.forward * -5);
+    }
+
+    void RotateRight()
+    {
+        transform.Rotate(Vector3.forward * 5);
+    }
+}
