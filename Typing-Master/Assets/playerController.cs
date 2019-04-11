ca444f226181279e8be4ed19f0ee8544134766b1
ahmedabuamra <ah.abuamra@gmail.com>
2019-04-10 20:21:11 -0700

    Collisins handler

--- Typing-Master/Assets/playerController.cs
+++ Typing-Master/Assets/playerController.cs
@@ -8,26 +8,47 @@ public class playerController : MonoBehaviour {
     
     public GameObject shot;
     public Transform ship;
-    public float targetX, targetY, angle;
+    public float angle;
     public Transform target;
+    private int targetIdx = 1;
     void Start ()
     {
     }
 	
 	void Update ()
     {
+
+        Debug.Log(EnemiesManager.enemyShips.Count);
         foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
         {
-            if (Input.GetKeyDown(vKey))
+            if(Input.GetKeyDown(vKey))
             {
-                Vector3 shotPos = ship.position;
-                shotPos.y += 0.5f;
-                Instantiate(shot, shotPos, ship.rotation);
+                if(targetIdx == -1)
+                {
+                    for(int i = 0; i < EnemiesManager.enemyShips.Count; i++)
+                     {
+                       /* if (Input.GetKeyDown(vKey).Equals(EnemiesManager.enemyShips.shpot[0p]) )
+                        {
+                            Vector3 shotPos = ship.position;
+                            shotPos.y += 0.5f;
+                            Instantiate(shot, shotPos, ship.rotation); 
+                        }*/
+
+                    }
+
+                }
+                else
+                {
+                
+                    target = EnemiesManager.enemyShips[0].transform;
+                    Vector3 shotPos = ship.position;
+                    shotPos.y += 0.5f;
+                    rotate();
+                    Instantiate(shot, shotPos, ship.rotation);
+
+                }
             }
         }
-
-        if (Input.GetKeyDown("left"))
-            rotate();
     }
 
     void rotate()
